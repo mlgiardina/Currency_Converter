2512baeb769fedd0154b1bd424999178d667c6df
Matthew Giardina <mlgiardina@me.com>
2015-05-21 18:29:02 -0500

    currency converter now initializes with hash

--- lib/CurrencyConverter.rb
+++ lib/CurrencyConverter.rb
@@ -1,2 +1,7 @@
 class CurrencyConverter
+  attr_accessor :exchange_rates
+
+  def initialize(hash)
+    @exchange_rates = hash
+  end
 end
--- spec/CurrencyConverterTest.rb
+++ spec/CurrencyConverterTest.rb
@@ -8,5 +8,14 @@ class CurrencyConverterTest < Minitest::Test
     assert(CurrencyConverter)
   end
 
+  def test_currency_converter_has_hash
+    hash = {
+      usd: 1,
+      eur: 0.90
+    }
+    result = CurrencyConverter.new(hash)
+    assert(result.exchange_rates)
+  end
+
 end
 
