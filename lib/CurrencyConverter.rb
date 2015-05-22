077d4929e32aa00e327324c7277cddf938004908
Matthew Giardina <mlgiardina@me.com>
2015-05-22 15:55:38 -0500

    finished normal mode

--- lib/CurrencyConverter.rb
+++ lib/CurrencyConverter.rb
@@ -1,5 +1,8 @@
 require_relative "currency.rb"
 
+class UnknownCurrencyCodeError < StandardError
+end
+
 class CurrencyConverter
   attr_accessor :exchange_rates, :exchanged_currency
 
@@ -7,13 +10,26 @@ class CurrencyConverter
     @exchange_rates = hash
   end
 
+  def exchange_maths(old_rate, requested_rate)
+    @new_rate = requested_rate / old_rate
+  end
+
   def make_exchange(old_currency, requested_currency)
-    @exchanged_currency = Currency.new("",requested_currency)
+    @exchanged_currency = Currency.new(nil,requested_currency)
     @exchange_rates.each do |key, value|
       if key == requested_currency
-      @exchanged_currency.value = old_currency.value * value
+        @requested_rate = value
+      end
+      if key == old_currency.code
+        @old_rate = value
+      end
+      if @exchange_rates.has_key?(requested_currency) == false
+        raise UnknownCurrencyCodeError
       end
     end
+    exchange_maths(@old_rate, @requested_rate)
+    @exchanged_currency.value = @new_rate * old_currency.value
+    @exchanged_currency.value = @exchanged_currency.value.round(2)
     @exchanged_currency
   end
 end
--- spec/CurrencyConverterTest.rb
+++ spec/CurrencyConverterTest.rb
@@ -24,5 +24,27 @@ class CurrencyConverterTest < Minitest::Test
     assert_equal(exchanged_currency, Currency.new(9.0, :EUR))
   end
 
+  def test_can_convert_multiple_currencies
+    currency_converter = CurrencyConverter.new({:USD => 1, :EUR => 0.9, :GBP => 0.65, :INR => 63.48})
+    us_currency = Currency.new(10, :USD)
+    eur_currency = Currency.new(10, :EUR)
+    gbp_currency = Currency.new(10, :GBP)
+    inr_currency = Currency.new(10, :INR)
+    us_to_eur = currency_converter.make_exchange(us_currency, :EUR)
+    eur_to_gbp = currency_converter.make_exchange(eur_currency, :GBP)
+    gbp_to_inr = currency_converter.make_exchange(gbp_currency, :INR)
+    inr_to_us = currency_converter.make_exchange(inr_currency, :USD)
+    assert_equal(Currency.new(9,:EUR), us_to_eur)
+    assert_equal(Currency.new(7.22, :GBP), eur_to_gbp)
+    assert_equal(Currency.new(976.62, :INR), gbp_to_inr)
+    assert_equal(Currency.new(0.16, :USD), inr_to_us)
+  end
+
+  def test_raise_unknown_currency_error
+    currency_converter = CurrencyConverter.new({:USD => 1, :EUR => 0.9})
+    us_currency = Currency.new(10, :USD)
+    assert_raises(*UnknownCurrencyCodeError) { currency_converter.make_exchange(us_currency, :INR)}
+  end
+
 end
 
